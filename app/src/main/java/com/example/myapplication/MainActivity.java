f6529a94cd3cc0e5229309255d014c9afea2eaf1
Juanjo <juanjoserequenalama@gmail.com>
2021-03-07 21:03:16 +0100

    cambio muy importante

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //coment
         //coment debajo de comment
+        //probando otro comment por si acaso
     }
 }
