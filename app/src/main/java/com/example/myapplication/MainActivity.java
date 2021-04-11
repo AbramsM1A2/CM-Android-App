c554ade1e5ac3064ef433a7f062e5048e3d9898b
Rafael Delgado <rdelmar00@gmail.com>
2021-04-11 17:06:27 +0200

    premerge

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import com.example.myapplication.bottomMenu.cardsTab.CardsTabFragment;
@@ -24,7 +27,6 @@ public class MainActivity extends AppCompatActivity implements HomeFragment.onFr
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
         //Menu inferior
         BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
         navigation.bringToFront();
@@ -60,7 +62,6 @@ public class MainActivity extends AppCompatActivity implements HomeFragment.onFr
                 .commit();
 
     }
-
     /**
      * Inicia el repaso de cartas a partir del mazo seleccionado
      * @param dataItem
@@ -71,4 +72,5 @@ public class MainActivity extends AppCompatActivity implements HomeFragment.onFr
         intent.putExtra("message_key", dataItem.getNameText());
         startActivity(intent);
     }
+
 }
--- app/src/main/java/com/example/myapplication/bottomMenu/settingsTab/SettingsFragment.java
+++ app/src/main/java/com/example/myapplication/bottomMenu/settingsTab/SettingsFragment.java
@@ -1,7 +1,13 @@
 package com.example.myapplication.bottomMenu.settingsTab;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.myapplication.R;
@@ -11,5 +17,18 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.settings, rootKey);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+        //TODO
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Configuration config = getResources().getConfiguration();
+        //TODO
     }
 }
